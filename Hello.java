1b55470cdc29a0095645a690a58b62ea07bae6df
pragi <bmprakalya@gmail.com>
2023-01-21 06:28:53 +0530

    Third commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 // Hello.Java
 public class Hello{
 public static void main(String[] args){
-System.out.println("Hello, world from GIT!");
+System.out.println("Second time Git Welcome u!");
+System.out.println("SBEC");
+System.out.println("Changes after pushing to remote");
 }
 }
